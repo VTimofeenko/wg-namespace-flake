c77ad84a2d9f894cf9b5f4a0d0a3d447f752e399
Vladimir Timofeenko <id@vtimofeenko.com>
2025-05-28 15:39:32 -0700

    refactor!: remove nixpkgs input
    This flake only provides a module and does not depend on anything from
    nixpkgs.
    
    Consumers might see a warning about nonexistent input => marking as
    breaking.

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 {
   description = "A flake that implements Wireguard adapter that gets moved into a VPN namespace";
-  outputs = { self, nixpkgs }:
-    {
-      nixosModules.default = import ./modules;
-    };
+  outputs = _: {
+    nixosModules.default = import ./modules;
+  };
 }
